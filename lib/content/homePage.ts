708594bf27cbb8065264d9dc67faa2c12e35bf1b
Dbrowne14 <davidbrowne1992@gmail.com>
2026-06-30 19:43:08 +0100

    Streamlining getHomePage content functions

--- lib/content/homePage.ts
+++ lib/content/homePage.ts
@@ -4,21 +4,3 @@ export async function getHomePage() {
   return homePage;
 }
 
-export const getHeroFooter = async () => {
-  return homePage.hero.footer_Text;
-};
-
-export const getValueProp = async () => {
-  return homePage.valueProp;
-};
-export const getInsightsOverview = async () => {
-  return homePage.insightsOverview;
-};
-
-export const getAboutOverview = async () => {
-  return homePage.aboutOverview;
-};
-
-export const getServicesOverview = async () => {
-return homePage.servicesOverview
-}
